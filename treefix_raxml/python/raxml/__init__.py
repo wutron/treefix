Fixed p-value computation

treefix_raxml/python/raxml/__init__.py
@@ -86,8 +86,8 @@ class RAxML:
     def compute_lik_test(self, tree, test="SH", report="both"):
         """
         Computes the test statistic, returning the pvalue and Dlnl
-        report -- "over":  one-sided test, i.e. H0: LH_{tree} < LH_{input} v H1: LH_{tree} > LH_{input}
-                  "under": one-sided test, i.e. H0: LH_{tree} > LH_{input} v H1: LH_{tree} < LH_{input}
+        report -- "under":  one-sided test, i.e. H0: LH_{tree} > LH_{input} v H1: LH_{tree} < LH_{input}
+                  "over": one-sided test, i.e. H0: LH_{tree} < LH_{input} v H1: LH_{tree} > LH_{input}
                   "both":  two-sided test, i.e. H0: LH_{tree} == LH_{input} v H1: LH_{tree} != LH_{input}
         """
         ##use scipy.stats to determine whether zscore is significant
@@ -110,15 +110,13 @@ class RAxML:
             self.read_tree(tree)
             zscore, Dlnl = raxml.compute_LH(self.adef, self.tr,
                                             self.best_LH, self.weight_sum, self.best_vector)
-            if Dlnl <= 0:
-                return 1.0, Dlnl
             
             # really should just use pval = norm.sf(zscore) if one of the trees is the ML tree, 
             # but SH test compares two a priori trees (to determine if T_x and T_y
             # are equally good explanations of the data), so raxml uses two-sided test
-            if report == "over":
+            if report == "under":
                 return norm.sf(zscore), Dlnl
-            elif report == "under":
+            elif report == "over":
                 return 1-norm.sf(zscore), Dlnl
             elif report == "both":
                 return norm.sf(abs(zscore))*2, Dlnl